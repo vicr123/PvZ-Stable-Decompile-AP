fad3a5e7c0f6d157d845fce28317a86123f05448
Victor Tran <vicr12345@gmail.com>
2026-09-17 20:23:28 +1000

    Fix crash when looking at plants in Almanac

--- Lawn/Plant.cpp
+++ Lawn/Plant.cpp
@@ -6594,7 +6594,7 @@ void Plant::Die(bool send_lawn_link)
         // Send LawnLink if required
         constexpr SeedType suicidal_plants[] = {SeedType::SEED_CHERRYBOMB, SeedType::SEED_GRAVEBUSTER, SeedType::SEED_ICESHROOM, SeedType::SEED_DOOMSHROOM, SeedType::SEED_JALAPENO, SeedType::SEED_BLOVER, SeedType::SEED_INSTANT_COFFEE};
     
-        if (ranges::find(suicidal_plants, mSeedType) == std::end(suicidal_plants) && !mApp->IsWallnutBowlingLevel() && mApp->mGameScene == GameScenes::SCENE_PLAYING && (mBoard->ChooseSeedsOnCurrentLevel(true) || mBoard->HasConveyorBeltSeedBank()))
+        if (mBoard && ranges::find(suicidal_plants, mSeedType) == std::end(suicidal_plants) && !mApp->IsWallnutBowlingLevel() && mApp->mGameScene == GameScenes::SCENE_PLAYING && (mBoard->ChooseSeedsOnCurrentLevel(true) || mBoard->HasConveyorBeltSeedBank()))
         {
             mApp->mAP->SendLawnLink({
                 APWrapper::LawnLinkAction::PlantRemoved,
